96af39b4b2809e3ab964125881ebd6622ba40e42
mayankchugh17 <mayankchugh222@gmail.com>
2025-04-08 00:53:28 +0530

    Adding and Deletion added

--- src/todoList.jsx
+++ src/todoList.jsx
@@ -3,17 +3,40 @@ import { v4 as uuidv4 } from 'uuid';
 
 export default function TodoList(){
    
-    let [todos, setTodos] = useState(["sample tasks"])
+    let [todos, setTodos] = useState([{task : "sample tasks", id : uuidv4()}])
+    let [newTodo, setNewTodo] = useState("");
+
+    let addNewTodo = () => {
+        setTodos([...todos, {task : newTodo , id : uuidv4()}])
+        setNewTodo("")
+    }
+
+    let updateTodoValue = (event) => {
+        setNewTodo(event.target.value);
+    }
+
+    let deleteTodo = (id) => {
+        setTodos(todos.filter((todo) => todo.id != id));
+    }
+
     return (
     <>
-        <input placeholder='add a task' />
+        <input placeholder='add a task' value={newTodo} onChange={updateTodoValue}/>
         <br /><br />
-        <button>Add task</button>
+        <button onClick={addNewTodo}>Add task</button>
         <br /><br /><br /><br /><br /><br />
         <hr />
-        <h4>tasks Todo</h4>
+        <h3>tasks Todo</h3>
         <ul>
-            <li></li>
+            {
+                todos.map((todo)=>(
+                    <li key={todo.id}> 
+                        <span>{todo.task}</span>    &nbsp;&nbsp;&nbsp;
+                        <button onClick={()=>deleteTodo(todo.id)}>delete</button>
+                    </li>
+                ))
+            }
+            <br /><br />
         </ul>
     </>
    );
