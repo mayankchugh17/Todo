429ab3218a1a2459b83cb22b942e07fe025c2420
mayankchugh17 <mayankchugh222@gmail.com>
2025-04-07 23:53:36 +0530

    Adding an array

--- src/todoList.jsx
+++ src/todoList.jsx
@@ -2,34 +2,19 @@ import { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid';
 
 export default function TodoList(){
-    let [todos, setTodos] = useState([{task:"sample tasks", id:uuidv4()}]);
-    let [newTask, setNewTodo] = useState();
-
-    const addNewTask = () =>{
-        setTodos([...todos, newTask]);
-        setNewTodo("");
-    };
-    const updateTodoValue = (event) =>{
-        setNewTodo(event.target.value);             //here, newTask is being set
-    };
+   
+    let [todos, setTodos] = useState(["sample tasks"])
     return (
-
-        <div>
-            <input type="text" placeholder="Add a Task" value={newTask} onChange={updateTodoValue}/>
-            <br /><br />
-            <button onClick={addNewTask}>Add</button>
-            <br /><br />
-    
-            <hr />
-            <h3>Tasks Todo</h3>
-            <ul>
-                {
-                    todos.map((todo)=>(
-                        <li key={todo.id}>{todo.task}</li>
-                    ))
-                }
-            </ul>
-        </div>
-
-)
+    <>
+        <input placeholder='add a task' />
+        <br /><br />
+        <button>Add task</button>
+        <br /><br /><br /><br /><br /><br />
+        <hr />
+        <h4>tasks Todo</h4>
+        <ul>
+            <li></li>
+        </ul>
+    </>
+   );
 }
